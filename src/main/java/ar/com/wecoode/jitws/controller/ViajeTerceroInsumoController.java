d7e8e0fd401610f2e4d0842daea9d122b3db91b4
Blas Bravo <bravoblasd@gmail.com>
2018-09-05 18:27:49 -0300

    Desarrollo controladores.

---
+++ src/main/java/ar/com/wecoode/jitws/controller/ViajeTerceroInsumoController.java
@@ -0,0 +1,97 @@
+package ar.com.wecoode.jitws.controller;
+
+import ar.com.wecoode.jitws.constant.RutaConstant;
+import ar.com.wecoode.jitws.exception.CodigoRespuesta;
+import ar.com.wecoode.jitws.exception.EstadoRespuesta;
+import ar.com.wecoode.jitws.exception.MensajeRespuesta;
+import ar.com.wecoode.jitws.model.ViajeTerceroInsumo;
+import ar.com.wecoode.jitws.service.ViajeTerceroInsumoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Clase ViajeTerceroInsumo Controller
+ * @author blas
+ */
+
+@RestController
+public class ViajeTerceroInsumoController {
+    
+    //Define la url
+    private final String URL = RutaConstant.URL_BASE + "/viajeterceroinsumo";
+    
+    //Crea una instancia del servicio
+    @Autowired
+    ViajeTerceroInsumoService elementoService;
+    
+    //Obtiene el siguiente id
+    @RequestMapping(value = URL + "/obtenerSiguienteId")
+    @ResponseBody
+    public int obtenerSiguienteId() {
+        return elementoService.obtenerSiguienteId();
+    }
+    
+    //Obtiene la lista completa
+    @RequestMapping(value = URL)
+    @ResponseBody
+    public List<ViajeTerceroInsumo> listar() {
+        return elementoService.listar();
+    }
+    
+    //Agrega un registro
+    @PostMapping(value = URL)
+    public ResponseEntity<?> agregar(@RequestBody ViajeTerceroInsumo elemento) {
+        try {
+            elementoService.agregar(elemento);
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.CREADO, 
+                    MensajeRespuesta.AGREGADO), HttpStatus.CREATED);
+        } catch(Exception e) {
+            //Retorna codigo y mensaje de error interno en el servidor
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.ERROR_INTERNO_SERVIDOR,
+                    MensajeRespuesta.ERROR_INTERNO_SERVIDOR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    //Actualiza un registro
+    @PutMapping(value = URL)
+    public ResponseEntity<?> actualizar(@RequestBody ViajeTerceroInsumo elemento) {
+        try {
+            elementoService.actualizar(elemento);
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.OK, 
+                    MensajeRespuesta.ACTUALIZADO), HttpStatus.OK);
+        } catch (ObjectOptimisticLockingFailureException oolfe) {
+            //Retorna codigo y mensaje de error de operacion actualizada por otra transaccion
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.TRANSACCION_NO_ACTUALIZADA,
+                    MensajeRespuesta.TRANSACCION_NO_ACTUALIZADA), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch(Exception e) {
+            //Retorna codigo y mensaje de error interno en el servidor
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.ERROR_INTERNO_SERVIDOR,
+                    MensajeRespuesta.ERROR_INTERNO_SERVIDOR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    //Elimina un registro
+    @DeleteMapping(value = URL)
+    public ResponseEntity<?> eliminar(@RequestBody ViajeTerceroInsumo elemento) {
+        try {
+            elementoService.eliminar(elemento);
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.OK, 
+                    MensajeRespuesta.ELIMINADO), HttpStatus.OK);
+        } catch(Exception e) {
+            //Retorna codigo y mensaje de error interno en el servidor
+            return new ResponseEntity(new EstadoRespuesta(CodigoRespuesta.ERROR_INTERNO_SERVIDOR, 
+                    MensajeRespuesta.ERROR_INTERNO_SERVIDOR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+}
