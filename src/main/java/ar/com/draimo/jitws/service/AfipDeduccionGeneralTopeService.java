48d3633c4ed81b4d50e393cc0e408589c4afc455
Cande Bertoldi <cande@draimo.com>
2019-08-23 13:04:31 -0300

    [AfipDeduccionGeneralTopeService] Cambio minimo.

--- src/main/java/ar/com/draimo/jitws/service/AfipDeduccionGeneralTopeService.java
+++ src/main/java/ar/com/draimo/jitws/service/AfipDeduccionGeneralTopeService.java
@@ -11,84 +11,86 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Servicio AfipDeduccionGeneralTope
+ *
  * @author blas
  */
-
 @Service
 public class AfipDeduccionGeneralTopeService {
-    
+
     //Define la referencia al dao
     @Autowired
     IAfipDeduccionGeneralTopeDAO elementoDAO;
-    
+
     //Define la referencia al dao de afipDeduccionGeneral
     @Autowired
     IAfipDeduccionGeneralDAO afipDeduccionGeneralDAO;
-    
+
     //Obtiene el siguiente id
     public int obtenerSiguienteId() {
         AfipDeduccionGeneralTope elemento = elementoDAO.findTopByOrderByIdDesc();
-        return elemento != null ? elemento.getId()+1 : 1;
+        return elemento != null ? elemento.getId() + 1 : 1;
     }
-    
+
     //Obtiene la lista completa
     public List<AfipDeduccionGeneralTope> listar() {
         return elementoDAO.findAll();
     }
-    
+
     //Obtiene la lista por anio
     public List<AfipDeduccionGeneralTope> listarPorAnio(short anio) {
         return elementoDAO.findByAnioOrderByAfipDeduccionGeneral_Id(anio);
     }
-    
+
     //Obtiene una lista por Descripcion
     public List<AfipDeduccionGeneralTope> listarPorDescripcion(String descripcion) {
-        if(descripcion.equals("***")) {
+        if (descripcion.equals("***")) {
             return elementoDAO.findAll();
         } else {
             return elementoDAO.findByDescripcionContaining(descripcion);
         }
     }
-    
+
     //Agrega un registro
     @Transactional(rollbackFor = Exception.class)
     public AfipDeduccionGeneralTope agregar(AfipDeduccionGeneralTope elemento) throws Exception {
         elemento = formatearStrings(elemento);
         String anio = String.valueOf(elemento.getAnio());
         //Obtiene longitud de anio, si supera 4 retorna error
-        if (anio.length()>4 || anio.length()<4) {
+        if (anio.length() > 4 || anio.length() < 4) {
             throw new DataIntegrityViolationException("Cantidad caracteres incorrecta en AÑO");
-        } 
-        if(elementoDAO.findByAnioAndAfipDeduccionGeneralOrderByAfipDeduccionGeneral_Id(
-                elemento.getAnio(), elemento.getAfipDeduccionGeneral()).isEmpty()){
+        }
+        if (elementoDAO.findByAnioAndAfipDeduccionGeneralOrderByAfipDeduccionGeneral_Id(
+                elemento.getAnio(), elemento.getAfipDeduccionGeneral()).isEmpty()) {
             return elementoDAO.saveAndFlush(elemento);
         } else {
             throw new DataIntegrityViolationException("Deducción General existente para el año fiscal.");
         }
     }
-    
+
     //Actualiza un registro
     @Transactional(rollbackFor = Exception.class)
     public void actualizar(AfipDeduccionGeneralTope elemento) throws Exception {
         elemento = formatearStrings(elemento);
         String anio = String.valueOf(elemento.getAnio());
         //Obtiene longitud de anio, si supera 4 retorna error
-        if (anio.length()>4 || anio.length()<4) {
+        if (anio.length() > 4 || anio.length() < 4) {
             throw new DataIntegrityViolationException("Cantidad caracteres incorrecta en AÑO");
-        } 
-            elementoDAO.save(elemento);
+        }
+        elementoDAO.save(elemento);
     }
-    
+
     //Elimina un registro
     @Transactional(rollbackFor = Exception.class)
     public void eliminar(int elemento) {
         elementoDAO.deleteById(elemento);
     }
-    
+
     //Formatea los strings
     private AfipDeduccionGeneralTope formatearStrings(AfipDeduccionGeneralTope elemento) {
-        elemento.setDescripcion(elemento.getDescripcion().trim());
+        if (elemento.getDescripcion() != null) {
+            elemento.setDescripcion(elemento.getDescripcion().trim());
+        }
         return elemento;
     }
-    
+
 }
