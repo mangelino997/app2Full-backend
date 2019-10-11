3f9e39eaa6a8a61cbac28b024134d6612bcf6e18
Blas Bravo <blas@draimo.com>
2019-10-11 12:34:35 -0300

    [ConfiguracionVehiculo][ViajeRemito] Correcciones.

--- src/main/java/ar/com/draimo/jitws/dao/IConfiguracionVehiculoDAO.java
+++ src/main/java/ar/com/draimo/jitws/dao/IConfiguracionVehiculoDAO.java
@@ -7,6 +7,8 @@ import ar.com.draimo.jitws.model.TipoVehiculo;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Interfaz DAO ConfiguracionVehiculo
@@ -22,7 +24,9 @@ public interface IConfiguracionVehiculoDAO extends JpaRepository<ConfiguracionVe
     //Obtiene una lista por TipoVehiculo y MarcaVehiculo
     public List<ConfiguracionVehiculo> findByTipoVehiculoAndMarcaVehiculo(Optional<TipoVehiculo> TipoVehiculo, Optional<MarcaVehiculo> marcaVehiculo);
     
-    //Obtiene una lista por marca de vehiculo
-    public List<ConfiguracionVehiculo> findByMarcaVehiculo(MarcaVehiculo marcaVehiculo);
+    //Obtiene por filtro
+    @Query(value = "SELECT * FROM configuracionvehiculo WHERE ((:idTipoVehiculo = 0 or idTipoVehiculo=:idTipoVehiculo) "
+            + "and (:idMarcaVehiculo=0 or idMarcaVehiculo=:idMarcaVehiculo))", nativeQuery = true)
+    public List<ConfiguracionVehiculo> listarPorFiltros(@Param("idTipoVehiculo") int idTipoVehiculo, @Param("idMarcaVehiculo") int idMarcaVehiculo);
     
 }
