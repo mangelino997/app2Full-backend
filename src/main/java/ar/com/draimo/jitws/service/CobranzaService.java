1b8b4972cd3a9dd6b8f3ce721fbd2adc22c636ce
Cande Bertoldi <cande@draimo.com>
2019-11-25 13:04:34 -0300

    [CobranzaService] Control en agregar.

--- src/main/java/ar/com/draimo/jitws/service/CobranzaService.java
+++ src/main/java/ar/com/draimo/jitws/service/CobranzaService.java
@@ -2,9 +2,13 @@
 package ar.com.draimo.jitws.service;
 
 import ar.com.draimo.jitws.dao.ICobranzaDAO;
+import ar.com.draimo.jitws.dao.ICobranzaItemDAO;
 import ar.com.draimo.jitws.dao.IEmpresaDAO;
 import ar.com.draimo.jitws.dao.ITipoComprobanteDAO;
+import ar.com.draimo.jitws.dao.IVentaComprobanteDAO;
 import ar.com.draimo.jitws.model.Cobranza;
+import ar.com.draimo.jitws.model.CobranzaItem;
+import ar.com.draimo.jitws.model.VentaComprobante;
 import java.sql.Timestamp;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +35,14 @@ public class CobranzaService {
     @Autowired
     ITipoComprobanteDAO tipoComprobanteDAO;
 
+    //Define el dao de cobranzaItem
+    @Autowired
+    ICobranzaItemDAO cobranzaItemDAO;
+
+    //Define el dao de ventaComprobante
+    @Autowired
+    IVentaComprobanteDAO ventaComprobanteDAO;
+
     //Obtiene el siguiente id
     public int obtenerSiguienteId() {
         Cobranza elemento = elementoDAO.findTopByOrderByIdDesc();
@@ -52,7 +64,21 @@ public class CobranzaService {
     public Cobranza agregar(Cobranza elemento) {
         elemento.setFechaRegistracion(new Timestamp(new java.util.Date().getTime()));
         elemento.setTipoComprobante(tipoComprobanteDAO.findById(4).get());
-        return elementoDAO.saveAndFlush(elemento);
+        Cobranza c = elementoDAO.saveAndFlush(elemento);
+        if(!elemento.getCobranzaItems().isEmpty()) {
+            for (CobranzaItem cobranzaItem : elemento.getCobranzaItems()) {
+                if(cobranzaItem.getImporte()!=null) {
+                    VentaComprobante vtaCpte = ventaComprobanteDAO.findById(
+                            cobranzaItem.getVentaComprobante().getId()).get();
+                    vtaCpte.setImporteSaldo(cobranzaItem.getVentaComprobante().getImporteSaldo().subtract(
+                            cobranzaItem.getImporte()));
+                    ventaComprobanteDAO.save(vtaCpte);
+                }
+                cobranzaItem.setCobranza(c);
+                cobranzaItemDAO.saveAndFlush(cobranzaItem);
+            }
+        }
+        return c;
     }
 
     //Actualiza un registro
